9056cc6ba24ece0b964751673de08b249db2f346
Braden Shill <shill.braden@gmail.com>
2025-08-16 23:22:32 -0400

    created implementation compatible with go1.18

--- randpass.go
+++ randpass.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"crypto/rand"
 	"fmt"
 	"os"
+	"math/rand"
 )
 
+const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 func main() {
-	fmt.Fprintf(os.Stdout, rand.Text())
+	const maxSize = 10
+	chars := []byte(alpha)
+	out := [maxSize]byte{}
+
+	for i := 0 ; i < maxSize; i++ {
+		out[i] = chars[rand.Intn(len(alpha))]
+	}
+	fmt.Fprintf(os.Stdout, string(out[:]))
 }
